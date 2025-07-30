55df73acd1be901bd75e242c93f07bc669907ef7
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-30 18:15:10 +0100

    Add banner sections wrapper

---
+++ lib/gld-fluid-typography/gld-banner/BannerSectionWrapper.tsx
@@ -0,0 +1,33 @@
+import { cn } from "../util/tw/twUtils";
+
+interface BannerSectionWrapperProps extends React.ComponentProps<`section`> {
+    sfid?: string;
+    componentName?: string;
+    componentType?: string;
+    variantName?: string;
+    background?: string;
+}
+
+export default function BannerSectionWrapper({
+    sfid,
+    componentName,
+    componentType,
+    variantName,
+    background,
+    className,
+    ...props
+}: BannerSectionWrapperProps) {
+    return (
+        <section
+            data-component={componentName}
+            data-type={componentType}
+            data-variant={variantName}
+            className={cn(
+                'w-full min-w-[320px] h-fit',
+                `${background ? background : ''}`,
+                className
+            )}
+            {...props}
+        />
+    );
+}

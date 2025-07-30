f96f8f8f25838dd49a9d7b59e7bf34d0f51da666
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-30 18:25:42 +0100

    Add banner wrapper data props

--- lib/gld-fluid-typography/gld-banner/BannerSectionWrapper.tsx
+++ lib/gld-fluid-typography/gld-banner/BannerSectionWrapper.tsx
@@ -1,28 +1,21 @@
 import { cn } from "../util/tw/twUtils";
 
+type dataAttributeProps = Record<string,string>
+
 interface BannerSectionWrapperProps extends React.ComponentProps<`section`> {
-  sfid?: string;
-  componentName?: string;
-  componentType?: string;
-  variantName?: string;
+  dataAttributes?: dataAttributeProps;
   background?: string;
 }
 
 export default function BannerSectionWrapper({
-  sfid,
-  componentName,
-  componentType,
-  variantName,
+  dataAttributes = {'data-sfid': 'example-sfid'},
   background,
   className,
   ...props
 }: BannerSectionWrapperProps) {
   return (
     <section
-      data-sfid={sfid}
-      data-component={componentName}
-      data-type={componentType}
-      data-variant={variantName}
+      {...dataAttributes}
       className={cn(
         "w-full min-w-[320px] h-fit",
         `${background ? background : ""}`,

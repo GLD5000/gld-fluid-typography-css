d0a26996b3ed2e4e0fd7882a45a4534365daab26
Gareth Devlin <garethlouisdevlin@gmail.com>
2025-07-31 08:07:41 +0100

    Remove unneeded attributes

--- lib/gld-fluid-typography/gld-banner/BannerSectionWrapper.tsx
+++ lib/gld-fluid-typography/gld-banner/BannerSectionWrapper.tsx
@@ -1,24 +1,15 @@
 import { cn } from "../util/tw/twUtils";
 
-type dataAttributeProps = Record<string,string>
 
-interface BannerSectionWrapperProps extends React.ComponentProps<`section`> {
-  dataAttributes?: dataAttributeProps;
-  background?: string;
-}
 
 export default function BannerSectionWrapper({
-  dataAttributes = {'data-sfid': 'example-sfid'},
-  background,
   className,
   ...props
-}: BannerSectionWrapperProps) {
+}: React.ComponentProps<`section`>) {
   return (
     <section
-      {...dataAttributes}
       className={cn(
         "w-full min-w-[320px] h-fit",
-        `${background ? background : ""}`,
         className
       )}
       {...props}
